6401b68b0e92465b1b8222d8d13410554ed30cc0
Junyeong Park <jypark@iportfolio.co.kr>
2021-05-07 10:46:29 +0900

    Update test.js

--- test.js
+++ test.js
@@ -3,8 +3,10 @@ console.log("hello world");
 // wrong codes
 var a = "1";
 
-a.includes("1")
+a.includes('1')
 
 for (var i = 0; i< 100; i++) { 
- console.log("a" + i)
+ console.log('a' + i)
 }
+
+console.log(i)
